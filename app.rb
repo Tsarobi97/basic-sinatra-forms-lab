34d8384c23fd9261559b13b9a8dfe742b686376a
RobertObi <robobi97@googlemail.com>
2021-03-16 23:57:14 -0500

    commit tree 6060ff79c7e8d49858b1407f4b2a30f89d3f3309

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+
+        erb :newteam
+    end
+
+    post '/team' do
+        "#{params[:name]}"
+    end
+
+    post '/team' do
+     "#{params[:coach]}"
+
+    end
+
+
 
 end
